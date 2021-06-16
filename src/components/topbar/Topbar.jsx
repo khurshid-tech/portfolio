edb4357891828faae550a06713867c2daee05012
khurshid-tech <khurshidalam3786@gmail.com>
2021-06-17 00:46:34 +0530

    topbar update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,15 +7,15 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            genius.
+            Khurshid Alam
           </a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+44 924 12 74</span>
+            <span>+91 8010428723</span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>safak@genius.com</span>
+            <span>info@khurshid.in</span>
           </div>
         </div>
         <div className="right">
